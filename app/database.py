77b02e018be96a5bfc9ea6a4ebe3f13783afb134
Kyle <kyle@kylelee.dev>
2024-08-15 18:26:13 -0400

    update mongodb string

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://logging-mongodb:27019/logging_db")
+MONGODB_URL = os.getenv(
+    "MONGODB_URL",
+    "mongodb://logging-mongodb.hfu5mq8dph6eg.us-east-1.cs.amazonlightsail.com:27018/logging_db",
+)
 client = AsyncIOMotorClient(MONGODB_URL)
 database = client["logging_db"]
 log_collection = database["logs"]
